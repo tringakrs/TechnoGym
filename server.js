c4c518dc15f14fd49b89a8507c5ab0542c5ff1ff
Tringa Krasniqi <tringe.krs#gmail.com>
2023-06-06 21:44:21 +0200

    Fix: Code Refactor

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use(fileUpload({
 
 // Routes
 app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
+app.use('/api', require('./routes/productRouter'))
+app.use('/api', require('./routes/paymentRouter'))
 
 
 
